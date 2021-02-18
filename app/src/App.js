7c3e2a5d41542b0b08642c1eaf9c2b71413e7334
mwbrennan <mwb5@me.com>
2021-02-17 19:29:20 -0600

    mvp

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Jokes from './components/Jokes';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Yo</h1>
+      <Jokes />
+    </div>
+  );
+}
+
+export default App;
